fd66bc6a770fad99dd6319c50c589dc5d3ddf079
r3dcrosse <mrdavidwayman@gmail.com>
2016-01-28 20:02:36 -0800

    Milestone 2 in prog

--- Instagram/PhotosViewController.swift
+++ Instagram/PhotosViewController.swift
@@ -9,11 +9,12 @@
 import UIKit
 import AFNetworking
 
-class PhotosViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class PhotosViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UIScrollViewDelegate {
     
     @IBOutlet weak var tableView: UITableView!
     
     var photos: [NSDictionary]?
+    var isMoreDataLoading = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -36,7 +37,7 @@ class PhotosViewController: UIViewController, UITableViewDataSource, UITableView
                 if let data = dataOrNil {
                     if let responseDictionary = try! NSJSONSerialization.JSONObjectWithData(
                         data, options:[]) as? NSDictionary {
-                            NSLog("response: \(responseDictionary)")
+                            //NSLog("response: \(responseDictionary)")
                             self.photos = responseDictionary["data"] as! [NSDictionary]
                             self.tableView.reloadData()
 
@@ -82,8 +83,65 @@ class PhotosViewController: UIViewController, UITableViewDataSource, UITableView
             vc.photoUrl = imageURL
         }
     }
+    
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         tableView.deselectRowAtIndexPath(indexPath, animated: true)
+    }
+    
+    func scrollViewDidScroll(scrollView: UIScrollView) {
+        if !isMoreDataLoading {
+            isMoreDataLoading = true
+            
+            // Calculate the position of one screen length before the bottom of the results
+            let scrollViewContentHeight = tableView.contentSize.height
+            let scrollOffsetThreshold = scrollViewContentHeight - tableView.bounds.size.height
+            print("scrollViewDidLoad part 1 should see 2 if loading more data")
+            // When the user has scrolled past the threshold, start requesting
+            if(scrollView.contentOffset.y > scrollOffsetThreshold && tableView.dragging) {
+                isMoreDataLoading = true
+                print("2222222222222")
+                // ... Code to load more results ...
+                loadMoreData()
+            }
+        }
+    }
+    
+    func loadMoreData() {
+        // ... Create the NSURLRequest (myRequest) ...
+        let clientId = "e05c462ebd86446ea48a5af73769b602"
+        let url = NSURL(string:"https://api.instagram.com/v1/media/popular?client_id=\(clientId)")
+        let request = NSURLRequest(URL: url!)
+        
+        // Configure session so that completion handler is executed on main UI thread
+        let session = NSURLSession(
+            configuration: NSURLSessionConfiguration.defaultSessionConfiguration(),
+            delegate:nil,
+            delegateQueue:NSOperationQueue.mainQueue()
+        )
+        
+        let task : NSURLSessionDataTask = session.dataTaskWithRequest(request,
+            completionHandler: { (data, response, error) in
+                
+                // Update flag
+                self.isMoreDataLoading = false
+                
+                // ... Use the new data to update the data source ...
+                if let data = data {
+                    if let responseDictionary = try! NSJSONSerialization.JSONObjectWithData(
+                        data, options:[]) as? NSDictionary {
+                            //NSLog("response: \(responseDictionary)")
+                            print("Stuff check")
+                            self.photos = responseDictionary["data"] as! [NSDictionary]
+                            self.tableView.reloadData()
+                            
+                    }
+                }
+
+                
+                // Reload the tableView now that there is new data
+                self.tableView.reloadData()
+        });
+        task.resume()
     }
     
 }
